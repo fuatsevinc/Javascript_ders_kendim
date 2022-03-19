2908bee76f3f248a56508771dc04ed71f775b266
fuatsevinc <fuatsevinc@icloud.com>
2022-03-19 16:04:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 function App() {
   return (
     <div>
+      <Header/>
       <h1>Merhaba Fuat</h1>
       <p>Buarada yeni yazilim</p>
       <a href="#anasayfa">Anasayfa</a> 
+      h3
     </div>
   );
 }
 
 export default App;
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-
+import header from "./header.js";
 ReactDOM.render(<App />, document.getElementById("root"));
