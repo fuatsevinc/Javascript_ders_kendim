7bd4f24a6aba12b61cf61d7cb27cf146388b051d
fuatsevinc <fuatsevinc@icloud.com>
2022-03-22 23:28:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./Header.js";
 import Footer from "./Footer.jsx";
 import Msg from "./Msg.jsx";
+import Clock from "./Clock.jsx";
 
 function App() {
   return (
     <div>
       <Header/>
       <Msg/>
+      <Clock/>
       <Footer/>
     </div>
   );
